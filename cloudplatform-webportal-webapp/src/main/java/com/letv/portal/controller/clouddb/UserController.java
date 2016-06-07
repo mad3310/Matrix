71f1fccdb5b7dfa538159695ad03c9203043452e
lisuxiao <lisuxiao@CNBJWK43997G51.letv.local>
2016-06-07 13:58:44 +0800

    1、物理机集群新增地域概念；2、添加根据session查询user对象接口

--- cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/clouddb/UserController.java
+++ cloudplatform-webportal-webapp/src/main/java/com/letv/portal/controller/clouddb/UserController.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.letv.common.result.ResultObject;
+import com.letv.common.session.SessionServiceImpl;
+import com.letv.portal.model.UserModel;
 import com.letv.portal.service.IUserService;
 
 @Controller
@@ -18,6 +21,8 @@ import com.letv.portal.service.IUserService;
 public class UserController {
 	@Resource
 	private IUserService  userService;
+	@Autowired
+	private SessionServiceImpl sessionService;
 
 	
 	private final static Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -26,6 +31,14 @@ public class UserController {
 	public @ResponseBody ResultObject getUserById(@PathVariable Long userId) {
 		ResultObject obj = new ResultObject();
 		obj.setData(this.userService.getUserById(userId));
+		return obj;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)   
+	public @ResponseBody ResultObject getUserBySession(ResultObject obj) {
+		Long userId = this.sessionService.getSession().getUserId();
+		UserModel u = this.userService.getUserById(userId);
+		obj.setData(u);
 		return obj;
 	}
 	
