e87cd661c4c305e82df813ee3f04ddf4d014e3fe
lisuxiao <lisuxiao@CNBJWK43997G51.letv.local>
2016-09-12 14:34:08 +0800

    集群状态添加部分注释

--- cloudplatform-webportal-data-model/src/main/java/com/letv/portal/enumeration/MclusterStatus.java
+++ cloudplatform-webportal-data-model/src/main/java/com/letv/portal/enumeration/MclusterStatus.java
@@ -5,20 +5,20 @@ public enum MclusterStatus implements ByteEnum{
 	RUNNING(1),  
 	BUILDDING(2),
 	BUILDFAIL(3),
-	AUDITFAIL(4),
+	AUDITFAIL(4),//审核失败
 	STARTING(7),
 	STOPPING(8),
 	STOPED(9),
-	DESTROYING(10),
-	DESTROYED(11),
-	DESTROYFAILED(19),
+	DESTROYING(10),//集群删除中
+	DESTROYED(11),//集群已删除
+	DESTROYFAILED(19),//集群删除失败
 	NOTEXIT(12),
 	DANGER(13),
 	CRISIS(14),
-	ADDING(15),
-    ADDINGFAILED(16),
-	DELETING(17),
-    DELETINGFAILED(18);
+	ADDING(15),//扩容中
+    ADDINGFAILED(16),//扩容失败
+	DELETING(17),//缩容中
+    DELETINGFAILED(18);//缩容失败
 	
 	private final Integer value;
 	
